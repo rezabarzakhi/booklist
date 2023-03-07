3048f72a316d054400e0d9eb70d3565d04f13e6d
rezabarzakhi <reza.brzkhi@gmail.com>
2023-03-07 04:33:53 -0800

    Update Header.js

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,13 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export default class Header extends Component {
-    render() {
-        return (
-            <h1 className="display-4 text-center">
-                <i className="fas fa-book-open text-warning">
-                </i> SabzLearn Js
-                <span className="text-warning">Book</span>List
-            </h1>
-        )
-    }
+  render() {
+    return (
+      <h1 className="display-4 text-center">
+        <i className="fas fa-book-open text-warning"></i> Barzakhi Js
+        <span className="text-warning">Book</span>List
+      </h1>
+    );
+  }
 }
